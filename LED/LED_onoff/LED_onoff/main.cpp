dd6e9cfc8d5310c29ddcd6adf52f5e0f3bd7a042
sayho <munchunii3@gmail.com>
2018-05-02 11:44:07 +0900

    LED shift

--- LED/LED_onoff/LED_onoff/main.cpp
+++ LED/LED_onoff/LED_onoff/main.cpp
@@ -16,10 +16,21 @@ int main(void)
 	
 	while (1) 
     {
-		PORTD = 0x00;
-		_delay_ms(500);
-		PORTD = 0xff;
-		_delay_ms(500);
+		for(int i=0; i<7; i++)
+		{
+			PORTD = ~(0x01 << i);
+			_delay_ms(500);
+		}
+		for(int j=0; j<7; j++)
+		{
+			PORTD = ~(0x80 >> j);
+			_delay_ms(500);
+		}
+		
+// 		PORTD = 0x00;
+// 		_delay_ms(500);
+// 		PORTD = 0xff;
+// 		_delay_ms(500);
     }
 }
 
